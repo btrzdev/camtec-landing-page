bf2df2ccee071eb2396cee778dc5787f09778c64
btrzdev <profbeatrizssilva@gmail.com>
2023-01-09 21:21:59 -0300

    feat: home hero draft

---
+++ components/Hero.tsx
@@ -0,0 +1,19 @@
+const Hero = () => {
+  return (
+    <div className="w-full h-[907px] bg-gradient-to-b from-red-background via-pink-background to-orange-background">
+      <div>
+        <h2>A produtividade está em casa</h2>
+        <p className="text-[42px] text-white text-bold">
+          CAM BOX VIRTUAL PARA HOME OFFICE
+        </p>
+        <p>
+          A Cam Tecnologia tem a melhor solução para manter a alta produtividade
+          de sua empresa no período de Home Office: é CAM VIRTUAL PARA HOME
+          OFFICE.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
